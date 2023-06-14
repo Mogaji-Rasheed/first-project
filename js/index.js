f93c82ad77aebf0b792a028b6807d8b3259ebb94
mogajirasheed <mogajirasheedatolagbe@gmail.com>
2023-06-13 21:23:39 -0700

    project structured

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
  let url = "https://randomuser.me/api";
  let image = document.getElementById("image");
  let fullName = document.getElementById("fullname");
+ let Age = document.getElementById("age");
  let phoneNumber = document.getElementById("phoneNumber");
  let streetAddress = document.getElementById("streetAddress");
  let emailAddress = document.getElementById("emailAddress")
@@ -8,4 +9,21 @@
 
 
 
-const getInfo = async ()
+const getInfo = () => {
+   fetch(url).then(response =>{
+      return response.json()
+   }).then( data => {
+      console.log(data)
+   })
+};
+image.src 
+fullName
+Age
+phoneNumber
+streetAddress
+emailAddress
+
+
+submit.addEventListener("click", ()=>{
+   return getInfo() 
+})
