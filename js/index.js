14fd7b3f4184969deb928ed29ab2d32dfc5b848d
mogajirasheed <mogajirasheedatolagbe@gmail.com>
2023-06-13 18:49:17 -0700

    restructered

---
+++ js/index.js
@@ -0,0 +1,11 @@
+ let url = "https://randomuser.me/api";
+ let image = document.getElementById("image");
+ let fullName = document.getElementById("fullname");
+ let phoneNumber = document.getElementById("phoneNumber");
+ let streetAddress = document.getElementById("streetAddress");
+ let emailAddress = document.getElementById("emailAddress")
+ let submit = document.getElementById("submit-btn");
+
+
+
+const getInfo = async ()
